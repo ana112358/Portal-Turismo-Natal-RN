6c7dd212db4f32379f4c0c8b081e122e957018fe
Ana Paula Oliveira de Lima <130050929+ana112358@users.noreply.github.com>
2024-05-06 11:05:57 -0300

    carrossel automático com JavaScript

---
+++ javascript/carrossel.js
@@ -0,0 +1,22 @@
+let radio = document.querySelector('.manual_but');
+let cont = 1;
+console.log(radio);
+
+document.getElementById('botao1').checked = true;
+
+setInterval(() =>{
+    proximaImg()
+},5000);
+
+function proximaImg(){
+    cont++;
+    let anterior;
+    anterior = cont-1;
+
+    if (cont>3) {
+        cont = 1;
+    }
+    
+    document.getElementById('botao'+anterior).checked = false;
+    document.getElementById('botao'+cont).checked = true;
+}
